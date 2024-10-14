3bf65934a109d585681ceda05d09119362fa2c7a
certat1um <canvasworkst@gmail.com>
2024-10-14 15:30:40 +0300

    fix: render deploy package.json

--- knexfile.js
+++ knexfile.js
@@ -4,22 +4,31 @@ require('dotenv/config');
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
+  production: {
+    client: 'postgres',
+    connection: {
+      connectionString: process.env.EXTERNAL_DB_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
   development: {
     client: 'postgres',
-    connection: process.env.IS_LOCAL
-      ? {
-          host: process.env.DB_HOST,
-          port: process.env.DB_PORT,
-          user: process.env.DB_USER,
-          password: process.env.DB_PASS,
-          database: process.env.DB_NAME,
-        }
-      : {
-          connectionString: process.env.EXTERNAL_DB_URL,
-          ssl: {
-            rejectUnauthorized: false,
-          },
-        },
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: './database/migrations',
